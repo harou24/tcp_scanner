da975609501837019bde3aab9193a23c8a54c6a0
harou24 <haroutioun24@gmail.com>
2022-08-30 22:49:22 +0200

    inefficient concurrent scan with goroutines

--- basic_tcp_scanner.go
+++ basic_tcp_scanner.go
@@ -1,22 +1,22 @@
 package main
 
-import(
-    "fmt"
-    "net"
+import (
+	"fmt"
+	"net"
 )
 
-func main(){
-    for i := 1; i <= 1024; i++ {
-        addr := fmt.Sprintf("scanme.nmap.org:%d", i)
+func main() {
+	for i := 1; i <= 65535; i++ {
+		addr := fmt.Sprintf("127.0.0.1:%d", i)
 
-        // the Dial function connects to a server.
-        conn, err := net.Dial("tcp", addr)
+		// the Dial function connects to a server.
+		conn, err := net.Dial("tcp", addr)
 
-        if err != nil {
-            // port is closed or filtered.
-            continue
-        }
-        conn.Close()
-        fmt.Printf("%d open\n", i)
-    }
+		if err != nil {
+			// port is closed or filtered.
+			continue
+		}
+		conn.Close()
+		fmt.Printf("%d open\n", i)
+	}
 }
--- concurrent_tcp_scanner.go
+++ concurrent_tcp_scanner.go
@@ -1,8 +1,25 @@
 package main
 
-import ("fmt"
-        "net")
+import (
+	"fmt"
+	"net"
+	"sync"
+)
 
 func main() {
-    
+	var wg sync.WaitGroup
+	for j := 1; j <= 65535; j++ {
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			addr := fmt.Sprintf("127.0.0.1:%d", j)
+			conn, err := net.Dial("tcp", addr)
+			if err != nil {
+				return
+			}
+			conn.Close()
+			fmt.Printf("%d open\n", j)
+		}(j)
+	}
+	wg.Wait()
 }
