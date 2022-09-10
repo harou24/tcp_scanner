4e25dd568b33e5af62fd20c181901b25276abfb7
harou24 <haroutioun24@gmail.com>
2022-09-10 14:13:05 +0200

    linter

--- multi_channel_tcp_scanner.go
+++ multi_channel_tcp_scanner.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
-    "sort"
+	"sort"
 )
 
 /*
@@ -16,43 +16,43 @@ var URL_TO_SCAN = "127.0.0.1"
 
 func worker(ports chan int, results chan int) {
 	for p := range ports {
-        addr := fmt.Sprintf("%s:%d", URL_TO_SCAN, p)
-        conn, err := net.Dial("tcp", addr)
+		addr := fmt.Sprintf("%s:%d", URL_TO_SCAN, p)
+		conn, err := net.Dial("tcp", addr)
 
-        if err != nil {
-            results <- 0
-            continue
-        }
-        conn.Close()
-        results <- p
+		if err != nil {
+			results <- 0
+			continue
+		}
+		conn.Close()
+		results <- p
 	}
 }
 
 func main() {
 	ports := make(chan int, 100)
-    results := make(chan int)
-    var openPorts []int
+	results := make(chan int)
+	var openPorts []int
 
 	for i := 0; i < cap(ports); i++ {
 		go worker(ports, results)
 	}
-    go func() {
-        for i := 1; i <= 65535; i++ {
-            ports <- i
-        }
-    }()
+	go func() {
+		for i := 1; i <= 65535; i++ {
+			ports <- i
+		}
+	}()
 
-    for i := 0; i < 65535; i++ {
-        port := <-results
-        if port != 0 {
-            openPorts = append(openPorts, port)
-        }
-    }
+	for i := 0; i < 65535; i++ {
+		port := <-results
+		if port != 0 {
+			openPorts = append(openPorts, port)
+		}
+	}
 	close(ports)
-    close(results)
-    sort.Ints(openPorts)
+	close(results)
+	sort.Ints(openPorts)
 
-    for _, port := range openPorts {
-        fmt.Printf("%d open\n", port)
-    }
+	for _, port := range openPorts {
+		fmt.Printf("%d open\n", port)
+	}
 }
